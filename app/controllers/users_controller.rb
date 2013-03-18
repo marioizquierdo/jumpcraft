730f265dd9d5ecb8fd4e3fea7e5299a3c5f3604e
Mario Izquierdo <tothemario@gmail.com>
2013-03-17 22:04:02 -0700

    Create JSON API responses for users (just add .json to the URLs)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
+  respond_to :html, :json
 
   def index
     @users = User.all
+    respond_with @users
   end
 
   def show
     @user = User.find(params[:id])
+    respond_with @user
   end
 
 end
