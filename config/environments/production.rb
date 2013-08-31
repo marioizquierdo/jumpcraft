8bda24f9469a952e2c68597251baa51fd5b16991
Mario Izquierdo <tothemario@gmail.com>
2013-08-30 18:41:04 -0700

    Configure sendgrid email to send passwords

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,15 +70,13 @@ Infiltration::Application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
+    address: "smtp.sendgrid.net",
+    port: '587',
+    domain: "heroku.com",
+    authentication: :plain,
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD']
   }
-
-
 
 end
