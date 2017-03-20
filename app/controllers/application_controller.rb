ab32df70b98501f809398090c78287210cf28ed0
Mario Izquierdo <tothemario@gmail.com>
2017-03-19 20:16:20 -0700

    Add devise configure_permitted_parameters for Rails 4 sanitization, to allow creating a user with a name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery # skip csrf token verification to make more easy to login from Flash
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+protected
 
-private
+  # Rails 4 moved the parameter sanitization from the model to the controller, causing Devise to handle this concern at the controller as well.
+  # This allows to signup users with the extra parameter: name
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
 
   # Calculate the @offset used for Mongoid skip from params[:page] and @page_size
   # Note: set @page_size before if you don't want to use default page size
